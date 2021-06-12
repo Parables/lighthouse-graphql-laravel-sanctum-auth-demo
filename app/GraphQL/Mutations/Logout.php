c7c7662c711495890eb5584b1c0422f65939d75c
parables <parables95@gmail.com>
2021-06-12 14:10:44 +0000

    Fixed logout logic

--- app/GraphQL/Mutations/Logout.php
+++ app/GraphQL/Mutations/Logout.php
@@ -3,15 +3,22 @@
 namespace App\GraphQL\Mutations;
 
 use App\Models\User;
+use GraphQL\Type\Definition\ResolveInfo;
 use Illuminate\Support\Facades\Auth;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class Logout
 {
     /**
-     * @param  null  $_
-     * @param  array<string, mixed>  $args
+     * Return a value for the field.
+     *
+     * @param  @param  null  $root Always null, since this field has no parent.
+     * @param  array<string, mixed>  $args The field arguments passed by the client.
+     * @param GraphQLContext $context Shared between all fields.
+     * @param ResolveInfo $resolveInfo Metadata for advanced query resolution.
+     * @return User
      */
-    public function __invoke($_, array $args): ?User
+    public function __invoke($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo): User
     {
         // Plain Laravel: Auth::guard()
         // Laravel Sanctum: Auth::guard(config('sanctum.guard', 'web'))
@@ -20,7 +27,7 @@ class Logout
         /** @var User|null $user */
         $user = $guard->user();
         $guard->logout();
-
+        $context->request->session()->invalidate();
         return $user;
     }
 }
