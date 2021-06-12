d7f2973c53715584ec924b7ba0d5ca2cf8ea469a
parables <parables95@gmail.com>
2021-06-12 08:40:23 +0000

    successfully setup sanctum, lighthouse-php and graphql-playground

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,16 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\NewAccessToken;
 
+
+/**
+ * @property string api_token
+ */
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -17,9 +23,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'api_token'
     ];
 
     /**
@@ -40,4 +47,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function  withToken(string $name, array $abilities = ['*']): User
+    {
+        $this->api_token = $this->createToken($name, $abilities)->plainTextToken;
+        $this->save();
+        return $this;
+    }
 }
--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'graphql',  'graphql-playground',  'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
